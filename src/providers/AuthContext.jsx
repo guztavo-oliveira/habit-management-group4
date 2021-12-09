bcb66d117df6934fb2c5e850b3618d6b738fddc4
RobsonMT <Rbsndev3@gmail.com>
2021-12-09 12:33:48 -0300

    feature: authprovider

---
+++ src/providers/AuthContext.jsx
@@ -0,0 +1,58 @@
+import { useState, createContext, useContext } from "react";
+import jwt_decode from "jwt-decode";
+import api from "../services/api";
+
+const AuthContext = createContext({});
+
+const useAuth = () => {
+  const context = useContext(AuthContext);
+  return context;
+};
+
+const AuthProvider = ({ children }) => {
+  const [data, setData] = useState(() => {
+    const id = localStorage.getItem("@gestaodehabitos:id");
+    const access = localStorage.getItem("@gestaodehabitos:access");
+
+    if (id && access) {
+      return { access, id };
+    }
+
+    return {};
+  });
+
+  const signIn = async (data) => {
+    const response = await api.post("/sessions/", data);
+
+    const { access } = response.data;
+
+    const { user_id } = jwt_decode(access);
+
+    localStorage.setItem("@gestaodehabitos:id", JSON.stringify(user_id));
+    localStorage.setItem("@gestaodehabitos:access", JSON.stringify(access));
+
+    setData({ user_id, access });
+  };
+
+  const signOut = () => {
+    localStorage.removeItem("@gestaodehabitos:id");
+    localStorage.removeItem("@gestaodehabitos:access");
+
+    setData({});
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        access: data.access,
+        id: data.id,
+        signIn,
+        signOut,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthProvider, useAuth };
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,8 +1,7 @@
 import { Route, Switch } from "react-router-dom";
-import { useState, useEffect } from "react";
 // import useLocalStorage from "use-local-storage";
-import { LandingPage } from "../pages/LandingPage";
-import { Dashboard } from "../pages/Dashboard";
+// import { LandingPage } from "../pages/LandingPage";
+// import { Dashboard } from "../pages/Dashboard";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 
