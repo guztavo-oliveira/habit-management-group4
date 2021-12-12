394182f3b40165e75faf48d40ad46da5660720ab
RobsonMT <Rbsndev3@gmail.com>
2021-12-12 18:24:38 -0300

    fix: provider habits

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,20 +1,20 @@
-import Button from "../../components/Button";
-import logoM from "../../assets/images/simbolo-color.png"
-import logoD from "../../assets/images/logo-color.png"
-import {Container,ImagemD,ImagemM,SectionB} from "./styles"
+import logoM from "../../assets/images/simbolo-color.png";
+import logoD from "../../assets/images/logo-color.png";
+import { Container, ImagemD, ImagemM, SectionB } from "./styles";
 import { useHistory } from "react-router";
+import { Button } from "@material-ui/core";
 const LandingPage = () => {
-    const history = useHistory();
-    
-    return (
-         <Container>
-             <ImagemM alt="logo" src={logoM}></ImagemM>
-             <ImagemD alt="logo" src={logoD}></ImagemD>
-             <SectionB>
-             <Button onClick={() => history.push("/login")}>LOGIN</Button>
-             <Button onClick={() => history.push("/signup")}>SIGNUP</Button>
-             </SectionB>
-         </Container>
-    )
-} 
+  const history = useHistory();
+
+  return (
+    <Container>
+      <ImagemM alt="logo" src={logoM}></ImagemM>
+      <ImagemD alt="logo" src={logoD}></ImagemD>
+      <SectionB>
+        <Button onClick={() => history.push("/login")}>LOGIN</Button>
+        <Button onClick={() => history.push("/signup")}>SIGNUP</Button>
+      </SectionB>
+    </Container>
+  );
+};
 export default LandingPage;
