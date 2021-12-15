5b7cd6180aab472220ea319d89107ac8de0ca851
Otávio Takashi Fuzeto Ikegami <otavinhoikgi@gmail.com>
2021-12-15 10:49:22 -0300

    feat: Logo text on mobile

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,7 +1,8 @@
 import Button from "../../components/Button";
-import logoM from "../../assets/images/simbolo-color.png"
-import logoD from "../../assets/images/logo-color.png"
-import {Container,ImagemD,ImagemM,SectionB} from "./styles"
+import logoM from "../../assets/images/simbolo-color.png";
+import logoD from "../../assets/images/logo-color.png";
+import logoText from "../../assets/images/logoText.png"
+import {Container,ImagemD,ImagemM,SectionB} from "./styles";
 import { useHistory } from "react-router";
 const LandingPage = () => {
     const history = useHistory();
@@ -9,6 +10,7 @@ const LandingPage = () => {
     return (
          <Container>
              <ImagemM alt="logo" src={logoM}></ImagemM>
+             <ImagemM alt="logoText" src={logoText}></ImagemM>
              <ImagemD alt="logo" src={logoD}></ImagemD>
              <SectionB>
              <Button onClick={() => history.push("/login")}>LOGIN</Button>
